4f87b702c7232c4cd48cb76fd33c6d9571d62ee5
Aras Azimi <aras.azimi@gmail.com>
2019-02-03 18:02:26 -0500

    Add time_it decorator in util module

---
+++ util/decorators.py
@@ -0,0 +1,13 @@
+from functools import wraps
+
+def time_it(func):
+    import time
+
+    @wraps(func)
+    def time_it_decorator(*args, **kwargs):
+        print(">ia> {} was called.".format(func.__name__))
+        start_time = time.time()
+        ret = func(*args, **kwargs)
+        print(">ia> {} took {} seconds".format(func.__name__, time.time()-start_time))
+        return ret
+    return time_it_decorator
