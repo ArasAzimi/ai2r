6977391cd89239e4cd50525b55e2548369d9c66a
Aras Azimi <aras.azimi@gmail.com>
2020-11-03 16:06:56 -0500

    Some more formatting

--- README.md
+++ README.md
@@ -1,38 +1,61 @@
 # AI for Airplane classification (ai2r)
 
+[[_TOC_]]
 ### Training
-Assuming that you have got the requirements setup on your local system, in a virtual environment or in a docker image; start by running `train.sh` with your desired parameters. 
-* `dataset_name` is the name of the dataset which has to be put in raw directory. If `raw` data does not exists or you want to use current ai2r dataset the datset will be downloaded and extracted in a new directory "datasets". You will see the details of this process in the prompt messages.
-* `model_name` is the name of a model to be used. This should be defined in models directory. Currently this can be set to one of:  
+Assuming that you have got the requirements setup on your local system, in a virtual environment or in a docker image; 
+start by running `train.sh` with your desired parameters. The assumption is that you would use a pre-trained model.
+* `--dataset` is the name of the dataset which has to be put in raw directory. If `raw` data does not exists or you want
+ to use current ai2r dataset; the dataset will be downloaded and extracted in a new directory "datasets". You will see 
+ the details of this process in the prompt messages.
+* `--model` is the name of a model to be used. This should be defined in models directory. Currently this can be set to 
+one of:  
   > inceptionv3_pretrained  
   > vgg16_pretrained  
+* `--gpu` is a boolean flag to allow gpu for the training or not
 
-For example: `python ai2r.py -d aircrafts -m inceptionv3_pretrained --gpu 1` will train the aircrafts dataset using inceptionv3 with trained weights from imagenet.     
+For example: `python ai2r.py -d aircrafts -m inceptionv3_pretrained --gpu 1` will train the aircrafts dataset using 
+inceptionv3 with trained weights from imagenet.     
 
-If this is the first time, this command will trigger downloading the aircrafts dataset and the corresponding labels in .npy format. Once download is complete the code will unzip the data and add it to a directory i.e., "datasets". The training will start with the configured parameters in "config.json". Once the training is done the results will be saved under out directory. The results include the trained model, binarized labels in pickle format, a summary of the training and a .png graph of how the training/validation progressed in different epochs
+When training is triggered for the first time; the aircrafts dataset will be downloaded along with the corresponding 
+labels in `.npy` format. Once download is complete the code will unzip the data and add it to a directory i.e., 
+"datasets". The training will start with the configured parameters in "config.json". Once the training is done the 
+results will be saved under out directory. The results include the trained model, binarized labels in pickle format, a 
+summary of the training and a .png graph of how the training/validation progressed in different epochs
 
 #### Training using docker:
 * `cd` to the ai2r project directory.
-* Specify the model and dataset to be used in `Train.sh` script.
-* Run `bash runDocker.sh gpu train`. This will run a docker container with the requirements installed. It will also mount the current directory to `/ai2r` directory in the docker so that the docker container has access to `Train.sh`. The docker image will run `Train.sh` in the same manner as if you run ai2r.py on your system or in virtual environment.
+* Specify the model and dataset to be used in `train.sh` script.
+* Run `bash runDocker.sh gpu train`. This will run a docker container with the requirements installed. It will also mount
+ the current directory to `/ai2r` directory in the docker so that the docker container has access to `Train.sh`. 
+ The docker image will run `Train.sh` in the same manner as if you run ai2r.py on your system or in virtual environment.
 * To use a docker image with CPU version of keras/tensorflow for training use `bash runDocker.sh cpu train`.
 
 ### Prediction
-Assuming some training is done and results are available under "out" directory, you can start by `python predict.py -i path_to_image`. This will scan the out directory for trained models and a prompt will ask user for the desired model to be used for prediction.
+Assuming some training is done and results are available under "out" directory, you can start by 
+`python predict.py -i path_to_image`. This will scan the out directory for trained models and a prompt will ask user 
+for the desired model to be used for prediction.
 
 #### Prediction using docker:
 * `cd` to the ai2r project directory.
 * Add the path to the image you would like to run a prediction on to the `Predict.sh` script.
-* Run `bash runDocker.sh`. This will run a docker container with the requirements installed. It will also mount the current directory to `/ai2r` directory in the docker so that the docker container has access to `Predict.sh`. The docker image will run `Predict.sh` in the same manner as if you run predict.py on your system or in virtual environment.
-* Running `bash runDocker.sh` by default will use a GPU version docker image and run prediction on the test image specified in `Predict.sh`. To use a docker image with CPU version of keras/tensorflow for prediction use `bash runDocker.sh cpu`
+* Run `bash runDocker.sh`. This will run a docker container with the requirements installed. It will also mount the 
+current directory to `/ai2r` directory in the docker so that the docker container has access to `Predict.sh`. 
+The docker image will run `Predict.sh` in the same manner as if you run predict.py on your system or in virtual environment.
+* Running `bash runDocker.sh` by default will use a GPU version docker image and run prediction on the test image 
+specified in `Predict.sh`. To use a docker image with CPU version of keras/tensorflow for prediction use `bash runDocker.sh cpu`
 
 ### Using the Flask api
-The code for the [Flask](http://flask.pocoo.org/) api is located under "api" directry. Any source code related to models training or prediction is on the root director of the project or under "src" directory. Trained models available to the api must be added to "deployment/model".
+The code for the [Flask](http://flask.pocoo.org/) api is located under "api" directry. Any source code related to 
+models training or prediction is on the root director of the project or under "src" directory. Trained models available 
+to the api must be added to "deployment/model".
 #### Testing Flask api
-The required [Postman](https://www.getpostman.com/) [collection](https://github.com/ArasAzimi/ai2r/blob/master/ai2r.postman_collection.json) and [environment](https://github.com/ArasAzimi/ai2r/blob/master/ai2r.postman_environment.json) are available in the root directory of the project. These files can be imported into Postman to test or try out the api.
+The required [Postman](https://www.getpostman.com/) [collection](https://github.com/ArasAzimi/ai2r/blob/master/ai2r.postman_collection.json)
+ and [environment](https://github.com/ArasAzimi/ai2r/blob/master/ai2r.postman_environment.json) are available in the
+  root directory of the project. These files can be imported into Postman to test or try out the api.
 
 ### Data
-The data is scraped from the web. Currently it contains samples for 13 types of airplanes mostly airlines. Data is not exactly balanced but it is good enough to fly!
+The data is scraped from the web. Currently it contains samples for 13 types of airplanes mostly airlines. Data is
+ not exactly balanced but it is good enough to fly!
 Here is a list of airplanes in the data:  
 
 |Type|Manufacturer|Code|#Samples|
--- ai2r.py
+++ ai2r.py
@@ -8,43 +8,46 @@ import argparse
 import json
 import os
 
-
 from util import check, hw_config
 from util.prep import *
 from util.postp import *
 
-def vgg16_pretrained(img_size,lb):
-	"""
-	This fucntion will take the pretrained VGG16 model from Keras and modify
-	the input size, number of classes, or both based on user input.
-	"""
-	from models.vgg import VGG16_pt
-	print(">ia> Building pretrained vgg16 model...")
-	model = VGG16_pt.build(width=img_size[0], height=img_size[1], depth=3, classes=len(lb.classes_))
-	return model
+
+def vgg16_pretrained(img_size, lb):
+    """
+    This function will take the pre-trained VGG16 model from Keras and modify
+    the input size, number of classes, or both based on user input.
+    """
+    from models.vgg import VGG16_pt
+    print(">ia> Building pre-trained vgg16 model...")
+    model = VGG16_pt.build(width=img_size[0], height=img_size[1], depth=3, classes=len(lb.classes_))
+    return model
+
 
 def inceptionv3_pretrained(img_size, lb):
-	"""
-	This function will take the pretrained InceptionV3 model from Keras and modify
-	the input size, number of classes, or both based on user input.
-	"""
-	from models.inception import InceptionV3_pt
-	print(">ia> Building pretrained inceptionV3 model...")
-	model = InceptionV3_pt.build(width=img_size[0], height=img_size[1], depth=3, classes=len(lb.classes_))
-	return model
+    """
+    This function will take the pre-trained InceptionV3 model from Keras and modify
+    the input size, number of classes, or both based on user input.
+    """
+    from models.inception import InceptionV3_pt
+    print(">ia> Building pre-trained inceptionV3 model...")
+    model = InceptionV3_pt.build(width=img_size[0], height=img_size[1], depth=3, classes=len(lb.classes_))
+    return model
+
 
 def select_model(argument, img_size, lb):
-	"""
-	To select a model based on user input through argument parser.
-	"""
-	if safe_str_cmp(argument , "inceptionv3_pretrained"):
-		model = inceptionv3_pretrained(img_size,lb)
-	elif safe_str_cmp(argument , "vgg16_pretrained"):
-		model = vgg16_pretrained(img_size, lb)
-	else:
-		return None
+    """
+    To select a model based on user input through argument parser.
+    """
+    if safe_str_cmp(argument, "inceptionv3_pretrained"):
+        model = inceptionv3_pretrained(img_size, lb)
+    elif safe_str_cmp(argument, "vgg16_pretrained"):
+        model = vgg16_pretrained(img_size, lb)
+    else:
+        return None
 
-	return model
+    return model
+
 
 def str2bool(v):
     """
@@ -58,143 +61,142 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
+
 def main():
-	# Construct argument parser and parse arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-d", "--dataset", required=True,	help="path to input dataset of images")
-	ap.add_argument("-m", "--model", required=True,	help="name of the model to be used for training")
-	ap.add_argument("--gpu", "--gpu", required=True,	help="name of generated files")
-	args = vars(ap.parse_args())
+    # Construct argument parser and parse arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dataset", required=True, help="path to input dataset of images")
+    ap.add_argument("-m", "--model", required=True, help="name of the model to be used for training")
+    ap.add_argument("--gpu", "--gpu", required=True, help="name of generated files")
+    args = vars(ap.parse_args())
 
-	dataset = args["dataset"]
-	model_name = args["model"]
-	datasets_dir = 'datasets'
-	raw_datasets_dir = 'raw'
-	RUN_GPU = str2bool(args["gpu"])
-	use_raw_data = False
+    dataset = args["dataset"]
+    model_name = args["model"]
+    datasets_dir = 'datasets'
+    raw_datasets_dir = 'raw'
+    RUN_GPU = str2bool(args["gpu"])
+    use_raw_data = False
 
-	# Reading configurations from config.json
-	with open('config.json') as json_config_file:
-		CONFIG = json.load(json_config_file)
+    # Reading configurations from config.json
+    with open('config.json') as json_config_file:
+        config = json.load(json_config_file)
 
-	# Retrive configuration paramters from the json file
-	GPU_ALLOCATION = 	CONFIG['train']['hw_resource']['USE_GPU']
-	test_size = 		CONFIG['train']['test_size']
-	img_w = 			CONFIG['train']['img_w']
-	img_h = 			CONFIG['train']['img_h']
-	learning_rate =	 	CONFIG['train']['learning_rate']
-	epochs = 			CONFIG['train']['epochs']
-	batch_size = 		CONFIG['train']['batch_size']
+    # Retrieve configuration parameters from the json file
+    gpu_allocation = config['train']['hw_resource']['USE_GPU']
+    test_size = config['train']['test_size']
+    img_w = config['train']['img_w']
+    img_h = config['train']['img_h']
+    learning_rate = config['train']['learning_rate']
+    epochs = config['train']['epochs']
+    batch_size = config['train']['batch_size']
 
-	downlaod_dataset_if_not_exists = CONFIG['dataset']['downlaod_ai2r_dataset']
-	if downlaod_dataset_if_not_exists:
-		use_raw_data = False
+    download_dataset_if_not_exists = config['dataset']['download_ai2r_dataset']
+    if download_dataset_if_not_exists:
+        use_raw_data = False
 
+    img_size = img_w, img_h
+    hw_config.configure_gpu_cpu(RUN_GPU, gpu_allocation)
 
-	img_size = img_w, img_h
-	hw_config.configure_gpu_cpu(RUN_GPU, GPU_ALLOCATION)
+    out_dir = 'out/' + dataset + '/' + model_name + '_e' + str(epochs) + '_lr' + str(learning_rate) + '_bs' + str(
+        batch_size) + '/'
+    model_path = out_dir + dataset
+    checkpoint_path = out_dir + "/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+    np_dataset = datasets_dir + '/' + dataset
 
-	out_dir = 'out/'+ dataset+ '/'+ model_name+ '_e'+ str(epochs)+ '_lr'+ str(learning_rate)+ '_bs'+ str(batch_size)+ '/'
-	model_path = out_dir+ dataset
-	checkpoint_path=out_dir+ "/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
-	np_dataset = datasets_dir+'/'+dataset
+    r = check.input_requirements(dataset, np_dataset, download_dataset_if_not_exists)
+    if not safe_str_cmp(r, "OK"):
+        print(">ia> Exited with error: {}".format(r))
+        return 0
 
-	r = check.input_requirements(dataset, np_dataset, downlaod_dataset_if_not_exists)
-	if not safe_str_cmp(r , "OK"):
-		print(">ia> Exited with error: {}".format(r))
-		return 0
+    if download_dataset_if_not_exists:
+        if not os.path.isfile(datasets_dir + '/aircrafts.npy'):
+            if not os.path.isfile('datasets.zip'):
+                download_dataset(datasets_dir)
+            extract_dataset(datasets_dir)
 
-	if downlaod_dataset_if_not_exists:
-		if os.path.isfile(datasets_dir+'/aircrafts.npy')== False:
-			if os.path.isfile('datasets.zip')== False:
-				download_dataset(datasets_dir)
-			extract_dataset(datasets_dir)
+    r = check.model_requirements(model_name, img_size)
+    if not safe_str_cmp(r, "OK"):
+        print(">ia> Exited with error: {}".format(r))
+        return 0
 
-	r = check.model_requirements(model_name, img_size)
-	if not safe_str_cmp(r , "OK"):
-		print(">ia> Exited with error: {}".format(r))
-		return 0
+    if not os.path.isdir(out_dir):
+        print('>ia> Creating output directory:' + out_dir)
+        os.makedirs(out_dir)
 
-	if os.path.isdir(out_dir) == False:
-		print('>ia> Creating output directory:'+ out_dir)
-		os.makedirs(out_dir)
+    if not os.path.isdir(datasets_dir):
+        print(">ia> Creating the datasets directory at: /{}".format(datasets_dir))
+        os.makedirs(datasets_dir)
 
-	if os.path.isdir(datasets_dir) == False:
-		print(">ia> Creating the datasets directory at: /{}".format(datasets_dir))
-		os.makedirs(datasets_dir)
+    if not os.path.isfile(np_dataset + '.npy'):
+        # if os.path.isdir(datasets_dir) == False:
+        print(">ia> Creating numpy dataset from the input...")
+        imagePaths = sorted(list(paths.list_images(raw_datasets_dir + '/' + dataset)))
+        data, labels = prepare_input(imagePaths, img_size)
+        data, labels = convert_np(data, labels)
+        data = normalize_input(data)
+        np.save(np_dataset, data)
+        np.save(np_dataset + '_lbs', labels)
+    else:
+        print(">ia> Loading previously processed input...")
+        data = np.load(np_dataset + '.npy')
+        labels = np.load(np_dataset + '_lbs.npy')
 
-	if os.path.isfile(np_dataset+'.npy')== False:
-		#if os.path.isdir(datasets_dir) == False:
-		print(">ia> Creating numpy dataset from the input...")
-		imagePaths = sorted(list(paths.list_images(raw_datasets_dir+'/'+dataset)))
-		data, labels = prepare_input(imagePaths, img_size)
-		data, labels = convert_np(data, labels)
-		data = normalize_input(data)
-		np.save(np_dataset, data)
-		np.save(np_dataset+'_lbs', labels)
-	else:
-		print(">ia> Loading previously processed input...")
-		data = np.load(np_dataset+'.npy')
-		labels = np.load(np_dataset+'_lbs.npy')
+    trainX, testX, trainY, testY = split_dataset(test_size, data, labels)
 
+    lb = LabelBinarizer()
+    trainY = lb.fit_transform(trainY)
+    testY = lb.transform(testY)
 
-	trainX, testX, trainY, testY = split_dataset(test_size, data, labels)
+    model = select_model(model_name, img_size, lb)
+    if model is None:
+        print(">ia> Exited with error: model_error")
+        return 0
 
+    # Initialize model and optimizer ( use binary_crossentropy for 2-class classification)
+    opt = SGD(lr=learning_rate, decay=learning_rate / epochs)
+    model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
-	lb = LabelBinarizer()
-	trainY = lb.fit_transform(trainY)
-	testY = lb.transform(testY)
+    # Train the model
+    train_datagen = ImageDataGenerator(
+        rescale=1,
+        rotation_range=30,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        fill_mode="nearest",
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
 
-	model = select_model(model_name, img_size, lb)
-	if model == None:
-		print(">ia> Exited with error: model_error")
-		return 0
+    test_datagen = ImageDataGenerator(rescale=1)
 
-	# Initialize model and optimizer ( use binary_crossentropy for 2-class classification)
-	opt = SGD(lr=learning_rate, decay=learning_rate / epochs)
-	model.compile(loss="categorical_crossentropy", optimizer=opt,	metrics=["accuracy"])
+    train_generator = train_datagen.flow(
+        trainX,
+        trainY,
+        batch_size=batch_size)
 
-	# Train the model
-	train_datagen = ImageDataGenerator(
-	        rescale=1,
-			rotation_range=30,
-			width_shift_range=0.1,
-			height_shift_range=0.1,
-			fill_mode="nearest",
-	        shear_range=0.2,
-	        zoom_range=0.2,
-	        horizontal_flip=True)
+    validation_generator = test_datagen.flow(
+        testX,
+        testY,
+        batch_size=batch_size)
 
-	test_datagen = ImageDataGenerator(rescale=1)
+    model_fit = model.fit_generator(
+        train_generator,
+        steps_per_epoch=len(trainX) // batch_size,
+        epochs=epochs,
+        validation_data=validation_generator,
+        validation_steps=len(testX) // batch_size,
+        callbacks=None)
 
-	train_generator = train_datagen.flow(
-	        trainX,
-			trainY,
-			batch_size=batch_size)
+    # Evaluate the network and save the report to eval_report
+    print(">ia> Evaluating network...")
+    predictions = model.predict(testX, batch_size=batch_size)
+    eval_report = classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=lb.classes_)
 
-	validation_generator = test_datagen.flow(
-			testX,
-			testY,
-			batch_size=batch_size)
+    print(eval_report)
+    # plot the training loss and accuracy
+    plot_save_results(model_fit, model_path)
+    save_model(model, model_path, lb, eval_report, config)
 
-	model_fit= model.fit_generator(
-	        train_generator,
-	        steps_per_epoch=len(trainX) // batch_size,
-	        epochs=epochs,
-	        validation_data=validation_generator,
-	        validation_steps=len(testX) // batch_size,
-			callbacks=None)
-
-	# Evaluate the network and save the report to eval_report
-	print(">ia> Evaluating network...")
-	predictions = model.predict(testX, batch_size=batch_size)
-	eval_report = classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=lb.classes_)
-
-
-	print(eval_report)
-	# plot the training loss and accuracy
-	plot_save_results(model_fit, model_path)
-	save_model(model, model_path, lb, eval_report, CONFIG)
 
 # Python 3 does not need if __name__ == "__main__":
 if __name__ == "__main__":
